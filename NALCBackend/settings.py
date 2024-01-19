b89f3463630ba44de4fb8b9b3821d06964e3ecb9
John William Miones <johnwilliam.miones@cit.edu>
2024-01-19 09:54:41 +0800

    Add necessary details for heroku

--- NALCBackend/settings.py
+++ NALCBackend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
@@ -31,7 +33,7 @@ SECRET_KEY = 'django-insecure-s-c25l(g)6yq$n(^bdz*l&@&@@yw6k4=i8sy5z34m$&z5pe$w=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -152,6 +154,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
